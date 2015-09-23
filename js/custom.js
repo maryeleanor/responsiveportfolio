42ada8d84b34a163a5d28dbef556af86139a3cfc
Mary Casserly <marycasserly@gmail.com>
2015-09-23 03:34:23 -0700

    turned off randomizer

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(function() {
 });
 
 
-
+/*
 var isoOptions = {
   masonry: {
     columnWidth: 1
@@ -25,7 +25,7 @@ $( function() {
    $grid.isotope('shuffle');
     
 });
-
+*/
 
 //Isotope
 $(window).load(function(){
